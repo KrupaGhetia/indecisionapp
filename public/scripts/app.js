a73d34cebb204d7035a749667e77c2c06208efa4
KrupaGhetia <krupa.ghetia@mountblue.io>
2018-04-30 19:31:02 +0530

    indecision app started

---
+++ public/scripts/app.js
@@ -0,0 +1,81 @@
+'use strict';
+
+console.log('app.js is running');
+
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Hi, I am your subtitle',
+    options: ['one', 'two']
+};
+
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
+);
+
+var user = {
+    name: 'Krupa H Ghetia',
+    age: 23,
+    location: 'Bengaluru'
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
